337d59a83be057289c59d5c4f184d634eb0a46c4
Bastien Lemonnier <bastienlemonnier3630@gmail.com>
2024-01-08 23:44:53 +0100

    added datasource and config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DataSource } from 'typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log(process.env);
+  const AppDataSource = new DataSource({
+    type: 'mongodb',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10) || 27017,
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    entities: [],
+  });
+
+  AppDataSource.initialize()
+    .then(() => {
+      console.log('Data Source has been initialized!');
+    })
+    .catch((err) => {
+      console.error('Error during Data Source initialization', err);
+    });
+  await app.listen(parseInt(process.env.APP_PORT, 10) || 3000);
 }
 bootstrap();
