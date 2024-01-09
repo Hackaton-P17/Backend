6eb01737bc78e6d1c239d1ceda3a4fd273c1eb32
Giulio <gerardigiulio@gmail.com>
2024-01-09 20:14:16 +0100

    Plancton : Ajout des dto au patchdto

--- src/Modules/Plancton/Dtos/patchPlancton.dto.ts
+++ src/Modules/Plancton/Dtos/patchPlancton.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { ParameterEntity } from 'src/Modules/Parameters/parameters.entity';
 import { SeuilEntity } from '../seuil.entity';
+import { PostParameterDto } from 'src/Modules/Parameters/Dtos/postParameter.dto';
+import { PostSeuilDto } from './postSeuilDto';
 
 export class PatchPlanctonDto {
   @ApiProperty()
@@ -10,10 +12,10 @@ export class PatchPlanctonDto {
   name: string;
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  @ApiProperty()
-  parameters: ParameterEntity[];
+  @ApiProperty({type: () => [PostParameterDto]})
+  parameters: PostParameterDto[];
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  @ApiProperty()
-  seuils: SeuilEntity[];
+  @ApiProperty({type: () => [PostSeuilDto]})
+  seuils: PostSeuilDto[];
 }
