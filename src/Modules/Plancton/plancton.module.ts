f12e80bc1c39c608101756b99212be44fd5f321a
Giulio <gerardigiulio@gmail.com>
2024-01-09 19:27:41 +0100

    Plancton : Ajout des controller et services au module

--- src/Modules/Plancton/plancton.module.ts
+++ src/Modules/Plancton/plancton.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlanctonEntity } from './plancton.entity';
+import { PlanctonController } from './plancton.controller';
+import { PlanctonService } from './plancton.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PlanctonEntity])],
-  controllers: [],
-  providers: [],
+  controllers: [PlanctonController],
+  providers: [PlanctonService],
 })
 export class PlanctonModule {}
