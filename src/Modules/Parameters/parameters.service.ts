3ad488be5f93dbd709931369ce2dc41556015693
Bastien Lemonnier <bastienlemonnier3630@gmail.com>
2024-01-09 22:07:56 +0100

    aggregate parameters list

--- src/Modules/Parameters/parameters.service.ts
+++ src/Modules/Parameters/parameters.service.ts
@@ -9,7 +9,39 @@ export class ParametersService {
     @InjectRepository(PlanctonEntity)
     private readonly planctonRepository: MongoRepository<PlanctonEntity>,
   ) {}
-  async getAllParameters() {
-    return await this.planctonRepository.distinct('parameters', {});
+  async getAllParameters(): Promise<PlanctonEntity[]> {
+    return await this.planctonRepository
+      .aggregate([
+        { $unwind: '$parameters' },
+        { $project: { parameters: true, _id: false } },
+        {
+          $project: {
+            name: '$parameters.name',
+            unite: '$parameters.unite',
+            isPublic: '$parameters.isPublic',
+            type: '$parameters.type',
+          },
+        },
+        {
+          $group: {
+            _id: {
+              name: '$name',
+              unite: '$unite',
+              isPublic: '$isPublic',
+              type: '$type',
+            },
+          },
+        },
+        {
+          $project: {
+            name: '$_id.name',
+            unite: '$_id.unite',
+            isPublic: '$_id.isPublic',
+            type: '$_id.type',
+            _id: false,
+          },
+        },
+      ])
+      .toArray();
   }
 }
