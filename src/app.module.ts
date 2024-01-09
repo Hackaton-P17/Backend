55095cef4998a9e396b212414473d182d2e7fcb4
Bastien Lemonnier <bastienlemonnier3630@gmail.com>
2024-01-09 08:37:21 +0100

    modules creation / imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from 'config/configuration';
+import { NomenclatureModule } from './Modules/Nomenclature/nomenclature.module';
+import { ParametersModule } from './Modules/Parameters/parameters.module';
+import { PlanctonModule } from './Modules/Plancton/plancton.module';
+import { ReleveModule } from './Modules/Releve/releve.module';
+import { StationModule } from './Modules/Station/station.module';
 
 @Module({
   imports: [
@@ -11,6 +16,11 @@ import configuration from 'config/configuration';
       load: [configuration],
       isGlobal: true,
     }),
+    NomenclatureModule,
+    ParametersModule,
+    PlanctonModule,
+    ReleveModule,
+    StationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
