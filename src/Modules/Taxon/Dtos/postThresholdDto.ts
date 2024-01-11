9cbe17bde7db68b943e3dafc1e30ad5ec99f4f35
Giulio <gerardigiulio@gmail.com>
2024-01-11 11:16:06 +0100

    Correctif des threshold

--- src/Modules/Taxon/Dtos/postThresholdDto.ts
+++ src/Modules/Taxon/Dtos/postThresholdDto.ts
@@ -1,12 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { ThresholdType } from '../threshold.entity';
-import { IsHexColor, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsHexColor, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class PostThresholdDto {
   @ApiProperty()
+  @IsOptional()
   minimum: number |null;
 
   @ApiProperty()
+  @IsOptional()
   maximum: number|null;
 
   @ApiProperty()
