632a0b793186f50d0bba5ea846bae312b7036a8a
Bastien Lemonnier <bastienlemonnier3630@gmail.com>
2024-01-11 10:09:55 +0100

    added getTaxons filter

--- src/Modules/Taxon/Dtos/getTaxons.dto.ts
+++ src/Modules/Taxon/Dtos/getTaxons.dto.ts
@@ -1,24 +1,13 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { ParameterEntity } from 'src/Modules/Parameters/parameters.entity';
-import { ThresholdEntity } from '../threshold.entity';
-import { IsNotEmpty } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsOptional } from 'class-validator';
+import { TaxonDisplayType } from '../taxon.entity';
 
 export class GetTaxonDto {
-  @ApiProperty()
-  @IsNotEmpty()
-  _id: string;
-
-  @ApiProperty()
-  @IsNotEmpty()
+  @ApiPropertyOptional()
+  @IsOptional()
   name: string;
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  @ApiProperty()
-  @IsNotEmpty()
-  parameters: ParameterEntity[];
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  @ApiProperty()
-  @IsNotEmpty()
-  thresholds: ThresholdEntity[];
+  @ApiPropertyOptional()
+  @IsOptional()
+  display: TaxonDisplayType;
 }
