a15cd7dda4adbd6cca1c15bc627ea44fc4be5925
Adam Gragnic <adamgragnic@gmail.com>
2024-01-12 13:46:58 +0100

    Fix pws temp

--- src/Modules/Auth/auth.service.ts
+++ src/Modules/Auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { UserService } from '../User/user.service';
@@ -14,10 +14,6 @@ export class AuthService {
     const user = await this.userService.getUserByUsername(username);
     if (user !== null) {
       const result = await bcrypt.compare(password, user.password);
-
-      if (!result) {
-        throw new HttpException('Bad credentiels');
-      }
 
       const payload = { role: user.role, id: user._id };
 
