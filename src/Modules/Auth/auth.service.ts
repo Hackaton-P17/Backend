2f7129991f23b07178612bd0a6e7a6b89c0a77e6
Bastien Lemonnier <bastienlemonnier3630@gmail.com>
2024-01-16 11:26:29 +0100

    updated login system

--- src/Modules/Auth/auth.service.ts
+++ src/Modules/Auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { UserService } from '../User/user.service';
+import { UserEntity } from '../User/user.entity';
 
 @Injectable()
 export class AuthService {
@@ -11,15 +12,29 @@ export class AuthService {
   ) {}
 
   async login(username: string, password: string) {
+    const logged = await this.getFittingAccount(username, password);
+    if (logged !== null) {
+      const payload = {
+        role: logged.role,
+        logged: logged._id,
+      };
+      return this.jwtService.signAsync(payload);
+    } else {
+      throw new Error('Le login a échoué, veuillez vérifier vos identifiants.');
+    }
+  }
+
+  private async getFittingAccount(
+    username: string,
+    password: string,
+  ): Promise<UserEntity | null> {
     const user = await this.userService.getUserByUsername(username);
     if (user !== null) {
-      const result = await bcrypt.compare(password, user.password);
-
-      const payload = { role: user.role, id: user._id };
-
-      return await this.jwtService.signAsync(payload);
+      return ((await bcrypt.compare(password, user.password)) as boolean)
+        ? user
+        : null;
     } else {
-      throw new Error('Le login à échoué, veuiller vérifier vos identifiants.');
+      return null;
     }
   }
 }
