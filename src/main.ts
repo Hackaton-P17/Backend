9a453a31d3a3e628462a8d3d1d45b7a0d8bb5ec0
Bastien Lemonnier <bastienlemonnier3630@gmail.com>
2024-03-15 10:57:56 +0100

    change cors origins acceptation

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: ['http://vps-764b086a.vps.ovh.net', 'http://92.222.216.95'],
   });
   app.useGlobalPipes(
     new ValidationPipe({
